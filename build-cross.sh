dc4f6bfbae8943bff6232767a7ef040978cebbfa
Tianon Gravi <admwiggin@gmail.com>
2016-06-28 13:56:29 -0700

    Use "--pull" in build-cross.sh

--- build-cross.sh
+++ build-cross.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-docker build -f Dockerfile.cross -t tianon/rawdns:cross .
+docker build --pull -f Dockerfile.cross -t tianon/rawdns:cross .
 
 rm -f rawdns*
 docker run --rm tianon/rawdns:cross sh -c 'cd /go/bin && tar -c rawdns*' | tar -xv
